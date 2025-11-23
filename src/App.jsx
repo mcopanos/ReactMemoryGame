510a32c5f3e5ee180b768c5865901c6abd3383ab
Michael Copanos <mcopanos@gmail.com>
2025-11-23 12:58:34 -0600

    feat(matching) match success/error handling

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,58 @@ import Board from './components/styled/board.styled.jsx'
 import List from './components/styled/list.styled.jsx'
 import Card from './components/styled/card.styled.jsx'
 import Item from './components/styled/item.styled.jsx'
-import { useId } from 'react'
-import { useState } from 'react'
+import { useId, useState, useMemo, useEffect } from 'react'
+import Emojis from './components/data/emojis.json'
+
 
 function App() {
-  const emojis = ['😍', '😍', '🥵', '🥵', '🥸', '🥸', '💩', '💩', '🥳','🥳', '🤑', '🤑'];
-  randomEmojis(emojis)
-  function randomEmojis(arr) {
-    arr.sort(() => Math.random() - 0.5)
-  }
+  const [emojis, setEmojis] = useState([...Emojis])
+  const [isClicked, setIsClicked] = useState([])
 
+  function sortCards(arr) {
+    arr.sort(() => Math.random() - 0.5);
+  };
+  
+  function toggleIsClicked(id) {
+    setEmojis((prevEmoji) =>
+      prevEmoji.map((emoji) =>
+        emoji === id ? { ...emoji, isClicked: true } : emoji
+      )
+    );
+
+  };
+  
+  if (isClicked.length === 2) {
+    const first = isClicked[0];
+    const second = isClicked[1];
+
+    if (first.emoji === second.emoji) {
+      // left off here. tyring to set is matched to tru
+      emojis.map((emoji) => 
+        emoji.isClicked === true ? emoji.isMatched = true : emoji);
+      
+      emojis.map((emoji) => 
+        emoji.isMatched === true ? emoji.isClicked = false : emoji);
+      
+      setIsClicked([]);
+    } else {
+      alert("Not a Match. Try again")
+      
+      emojis.map((emoji) => 
+        emoji.isClicked === true ? emoji.isClicked = false : emoji);
+
+      setIsClicked([]);
+    }
+  } 
+
+  useEffect(() => {
+    setIsClicked(emojis.filter((emoji) => emoji.isClicked))
+
+  }, [emojis])
+  
+  useMemo(() => {
+    return sortCards(emojis) 
+  }, []);
 
   return (
     <>
@@ -26,12 +68,22 @@ function App() {
           <List>
             {
               emojis.map(emoji => (
-                <Item key={useId()}>
-                  <Card>
-                    <text style={{fontSize: '4em'}}>{emoji}</text>
+                <Item 
+                  key={useId()}
+                  onClick={() => toggleIsClicked(emoji)}
+                >
+                  <Card id={useId()}> 
+                  {emoji.isMatched ? 
+                  <text 
+                      style={{fontSize: '4em'}}
+                    >{emoji.emoji}</text>
+                    :
+                    <text 
+                      className={!emoji.isClicked ? 'hidden' : null} 
+                      style={{fontSize: '4em'}}
+                    >{emoji.emoji}</text>}
                   </Card>
                 </Item>
-
               ))
             }
           </List>
--- src/components/styled/card.styled.jsx
+++ src/components/styled/card.styled.jsx
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
 const Card = styled.div`
+
+    background: #9156ea; //#9156eaa6
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     width: 150px;
     height: 200px;
-    background: #9156ea;
     border-radius: 10px;
     box-shadow:  -1px 1px 5px 5px rgba(0, 0, 0, 0.15);
 `
